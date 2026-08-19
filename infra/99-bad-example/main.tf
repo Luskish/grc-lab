1beb44ad000c343c93b22ab324da2d78fc363532
Luskish <76147806+Luskish@users.noreply.github.com>
2026-08-18 23:38:07 -0400

    add security group (should be blocked)

---
+++ infra/99-bad-example/main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Do not deploy this. It exists to be rejected.
+resource "aws_security_group" "very_bad" {
+  name        = "wide-open"
+  description = "SSH and RDP from anywhere"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3389
+    to_port     = 3389
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
